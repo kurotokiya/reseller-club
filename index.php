2806c3c7d8603e69a693288a68d31421c53a0881
kurotokiya <kurotokiya@gmail.com>
2015-07-13 12:12:24 +0800

    commit

--- index.php
+++ index.php
@@ -3,20 +3,11 @@
 require './vendor/autoload.php';
 
 $cfg = require './config.php';
-$rcapi = new ResellerClub\Domains\Register($cfg['userid'], $cfg['apikey']);
+$rcapi = new ResellerClub\Contacts\Search($cfg['userid'], $cfg['apikey']);
 
-$res = $rcapi->domain_name('ubo.pw')
-             ->years('1')
-             ->ns('f1g1ns1.dnspod.net')
-             ->ns('f1g1ns2.dnspod.net')
-             ->customer_id('13095433')
-             ->reg_contact_id('44860833')
-             ->admin_contact_id('44860833')
-             ->tech_contact_id('44860833')
-             ->billing_contact_id('44860833')
-             ->invoice_option()
-             ->purchase_privacy('true')
-             ->protect_privacy('true')
+$res = $rcapi->customer_id('13095433')
+             ->no_of_records('10')
+             ->page_no('1')
              ->response();
 
 echo $res."\n";
