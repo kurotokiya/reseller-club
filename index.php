b4c835a9fdfcb09af361289452c8fddf160bfb38
kurotokiya <kurotokiya@gmail.com>
2015-07-13 11:53:04 +0800

    commit

--- index.php
+++ index.php
@@ -5,13 +5,10 @@ require './vendor/autoload.php';
 $cfg = require './config.php';
 $rcapi = new ResellerClub\Domains\Register($cfg['userid'], $cfg['apikey']);
 
-$res = $rcapi->domain_name('ubostweb.com')
+$res = $rcapi->domain_name('ubo.pw')
              ->years('1')
-             ->ns('ns1.linode.com')
-             ->ns('ns2.linode.com')
-             ->ns('ns3.linode.com')
-             ->ns('ns4.linode.com')
-             ->ns('ns5.linode.com')
+             ->ns('f1g1ns1.dnspod.net')
+             ->ns('f1g1ns2.dnspod.net')
              ->customer_id('13095433')
              ->reg_contact_id('44860833')
              ->admin_contact_id('44860833')
